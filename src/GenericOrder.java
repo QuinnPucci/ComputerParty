62817473ac33fac25bfb4117cd8b9a8701533b33
Quinn Pucci <pucciquinn@gmail.com>
2026-02-04 06:50:56 -0700

    created beggining of generic class

---
+++ src/GenericOrder.java
@@ -0,0 +1,14 @@
+public class GenericOrder<T> {
+    T item;
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    void main() {
+    }
+}
