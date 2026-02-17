1d507a7ac43c300c807d83020b10c40529adb0a7
Quinn Pucci <pucciquinn@gmail.com>
2026-02-17 07:41:03 -0700

    started working on creating unique IDs for each container

--- src/GenericOrder.java
+++ src/GenericOrder.java
@@ -1,5 +1,14 @@
 public class GenericOrder<T> {
     T item;
+    // the below attributes are for supporting the unique ID of each container
+    private static int counter = 0; // private and static so it's restricted to this class, and part of the class not the instances
+    private final int id; // restricted to this class and cannot be changed
+
+    GenericOrder(T item){
+        this.item = item;
+        // everytime a container is constructed, up the counter and assign that number to the ID.
+        id = ++counter;
+    }
 
     public void setItem(T item) {
         this.item = item;
