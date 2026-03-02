7bb45a4b411c24c1f022b267511dcebef5a2cace
Quinn Pucci <pucciquinn@gmail.com>
2026-03-02 10:46:02 -0700

    modified and completed generic order class. this will be the super class of all the sub classes. decided to init as empty generic list in constructor

--- src/GenericOrder.java
+++ src/GenericOrder.java
@@ -1,23 +1,37 @@
+import java.util.*;
+
 public class GenericOrder<T> {
-    T item;
     // the below attributes are for supporting the unique ID of each container
     private static int counter = 0; // private and static so it's restricted to this class, and part of the class not the instances
-    private final int id; // restricted to this class and cannot be changed
+    private final int orderNum; // restricted to this class and cannot be changed
 
-    GenericOrder(T item){
-        this.item = item;
+    private List<T> items; // declare a list, init later in the constructor as an empty generic list
+
+    public GenericOrder(){
+        this.items = new ArrayList<T>(); // I decided to init as an empty list and use addItem method
         // everytime a container is constructed, up the counter and assign that number to the ID.
-        id = ++counter;
+        orderNum = ++counter;
     }
 
-    public void setItem(T item) {
-        this.item = item;
+    public void addItem(T item) {
+        items.add(item);
     }
 
-    public T getItem() {
-        return item;
+    // Maybe an addAll at some point
+
+    // getAllItems() as official “iteration access” method later (will just loop that copy in process())
+    public List<T> getAllItems() {
+        return new ArrayList<>(items); // returns a COPY for safety (calls cannot change the items)
     }
 
-    void main() {
+    public int getOrderNum() {
+        return orderNum;
     }
+
+    @Override
+    public String toString() {
+        List<T> view = getAllItems();
+        return "GenericOrder{orderNumber=" + orderNum + ", size=" + view.size() + ", items= " + view + "}" ;
+    }
+
 }
