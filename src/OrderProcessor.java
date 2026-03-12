8387b2a1429cfe641f251293341259adc2ca13c2
pucciquinn@gmail.com <m@ms-MacBook-Air.local>
2026-03-12 16:50:06 -0600

    worked on processor

--- src/OrderProcessor.java
+++ src/OrderProcessor.java
@@ -1,3 +1,16 @@
+import java.util.*;
+
 public class OrderProcessor {
+    public List<GenericOrder<?>> acceptedOrders = new ArrayList<>();
+    // declare and initiate an empty internal collection(list) of orders
+
+    public void accept(GenericOrder<?> order){
+        acceptedOrders.add(order); // add the order to the the list of orders
+    }
+
+    public void processor(List order){
+
+    }
+
 
 }
